f9fca1c05c5000c31c89f2af74ea0fb889d3c5da
Michael Silva <99063625+msil2@users.noreply.github.com>
2023-01-30 20:14:23 -0600

    Add files via upload

---
+++ lab3-example.sh
@@ -0,0 +1,13 @@
+# Author: Programmer
+# Copyright (c) 
+# Script follows here:
+
+echo -e "\n\nEnter a type of animal?"
+read ANIMAL
+echo -e "\nEnter a name?"
+read NAME
+echo -e "\nEnter a place?"
+read PLACE
+echo -e "\n\n$NAME is $ANIMAL and lives in $PLACE"
+
+
